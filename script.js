82d80686edaf3c32a6bedd45580efbacb4fc17a6
Oskar971 <ossta007@osloskolen.no>
2026-03-10 14:00:53 +0100

    hh
    hh

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const navLinks = document.querySelectorAll(".nav-links a");
-const menuToggle = document.getElementById("menu-toggle");
 
-navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    menuToggle.checked = false; // Lukk mobilmenyen
-  });
-});
